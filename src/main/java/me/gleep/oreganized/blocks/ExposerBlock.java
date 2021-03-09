8ad084b36aa26634b81762e101221d782ae3f835
GL33P_0R4NG3 <tom@lelek.eu>
2021-03-09 08:01:53 +0100

    transfer progres

--- src/main/java/me/gleep/oreganized/blocks/ExposerBlock.java
+++ src/main/java/me/gleep/oreganized/blocks/ExposerBlock.java
@@ -1,18 +1,22 @@
 package me.gleep.oreganized.blocks;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 import org.jetbrains.annotations.Nullable;
 
-public class ExposerBlock extends Block {
+import java.util.Random;
+
+public class ExposerBlock extends DirectionalBlock {
+    public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
+
     public ExposerBlock() {
         super(AbstractBlock.Properties.from(Blocks.OBSERVER));
     }
@@ -24,7 +28,30 @@ public class ExposerBlock extends Block {
 
     @Override
     public int getStrongPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
-        return ;
+        return blockState.getWeakPower(blockAccess, pos, side);
+    }
+
+    @Override
+    public int getWeakPower(BlockState blockState, IBlockReader blockAccess, BlockPos pos, Direction side) {
+        return 0;
+    }
+
+    public void tick(BlockState state, ServerWorld worldIn, BlockPos pos, Random rand) {
+        if (state.get(POWERED)) {
+            worldIn.setBlockState(pos, state.with(POWERED, Boolean.valueOf(false)), 2);
+        } else {
+            worldIn.setBlockState(pos, state.with(POWERED, Boolean.valueOf(true)), 2);
+            worldIn.getPendingBlockTicks().scheduleTick(pos, this, 2);
+        }
+
+        this.updateNeighborsInFront(worldIn, pos, state);
+    }
+
+    protected void updateNeighborsInFront(World worldIn, BlockPos pos, BlockState state) {
+        Direction direction = state.get(FACING);
+        BlockPos blockpos = pos.offset(direction.getOpposite());
+        worldIn.neighborChanged(blockpos, this, pos);
+        worldIn.notifyNeighborsOfStateExcept(blockpos, this, direction);
     }
 
     @Override
