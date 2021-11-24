bfcddec03a65bcbebd4eae96c8abfd68eb96515a
tom <tom@lelek.eu>
2021-11-24 09:36:57 +0100

    fixes in MoltenLeadBlock and updated textures

--- src/main/java/me/gleep/oreganized/blocks/MoltenLeadBlock.java
+++ src/main/java/me/gleep/oreganized/blocks/MoltenLeadBlock.java
@@ -7,44 +7,35 @@ import net.minecraft.core.Direction;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.tags.FluidTags;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.BucketPickup;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.gameevent.GameEvent;
-import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
-import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraftforge.client.IBlockRenderProperties;
 
 import javax.annotation.Nullable;
 import java.util.Optional;
-import java.util.function.Consumer;
 
 public class MoltenLeadBlock extends Block implements BucketPickup {
     public MoltenLeadBlock() {
-        super(BlockBehaviour.Properties.of((new Material.Builder(MaterialColor.COLOR_PURPLE)).noCollider().notSolidBlocking().nonSolid().liquid().build())
+        super(BlockBehaviour.Properties.of((new Material.Builder(MaterialColor.COLOR_PURPLE)).noCollider().nonSolid().build())
                 .strength(-1.0F, 3600000.0F)
                 .noDrops()
                 .requiresCorrectToolForDrops()
@@ -53,7 +44,7 @@ public class MoltenLeadBlock extends Block implements BucketPickup {
 
     @Override
     public boolean skipRendering(BlockState p_60532_, BlockState p_60533_, Direction p_60534_) {
-        return super.skipRendering(p_60532_, p_60533_, p_60534_);
+        return p_60533_.is(this) || super.skipRendering(p_60532_, p_60533_, p_60534_);
     }
 
     @Override
@@ -134,6 +125,16 @@ public class MoltenLeadBlock extends Block implements BucketPickup {
         }
 
         return Shapes.block();
+    }
+
+    @Override
+    public VoxelShape getVisualShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        return Shapes.empty();
+    }
+
+    @Override
+    public VoxelShape getOcclusionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos) {
+        return Shapes.empty();
     }
 
     /**
