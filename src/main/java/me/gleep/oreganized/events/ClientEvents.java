f9756cc901b2b28b0bd9398543d582ea37b7a8af
0rc1nus <mitchellctlui@gmail.com>
2022-04-09 13:55:49 +0800

    Removed unused imports

--- src/main/java/me/gleep/oreganized/events/ClientEvents.java
+++ src/main/java/me/gleep/oreganized/events/ClientEvents.java
@@ -2,11 +2,8 @@ package me.gleep.oreganized.events;
 
 import me.gleep.oreganized.Oreganized;
 import me.gleep.oreganized.client.model.ElectrumArmorModel;
-import me.gleep.oreganized.items.ElectrumArmorItem;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityRenderersEvent;
-import net.minecraftforge.client.event.RenderTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
